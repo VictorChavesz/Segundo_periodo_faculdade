77fef366a1a4f783bf4b8ab471c924ff50b314c8
VictorChavesz <152697740+VictorChavesz@users.noreply.github.com>
2024-04-01 15:22:40 -0300

    Add files via upload

--- Atividade2.java
+++ Atividade2.java
@@ -1,24 +1,21 @@
-package metodos1atividade;
-import java.util.Scanner;
-public class Atividade2 {
-    public static int sequenciaFibonacci(int escolha){
-        int resultadoEsperado = 0,primeiroN = 0, segundoN = 1, resultado = 0;
-        for(int i = 1; i < escolha-1; i++){
+package metodos;
 
-            resultado = primeiroN + segundoN;
-            primeiroN = segundoN;
-            segundoN = resultado;
-            resultadoEsperado = resultado;
-        }    
-        return resultado;
+import java.util.Scanner;
+
+public class Atividade2 {
+    
+    public static void numParImpar(int num){
+        if(num%2==0){
+            System.out.printf("%d é par",num);
+        }else{
+           System.out.printf("%d é impar",num); 
+        }
     }
     public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
+    Scanner ler = new Scanner(System.in); 
+     
+    System.out.println("informe um numero inteiro positivo");
+    numParImpar(ler.nextInt());
 
-    System.out.println("informe um valor");
-        int escolha = ler.nextInt();
-        sequenciaFibonacci(escolha);
-        System.out.println(sequenciaFibonacci(escolha));
-        ler.close();
     }
 }
